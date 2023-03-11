8568eda97ca774ff89f9f02a52467e64ee5d3853
tle321xx <125847689+tle321xx@users.noreply.github.com>
2023-03-11 17:32:05 +0700

    Add Homework Challenge 1, 2

---
+++ homeworkJS.js
@@ -0,0 +1,20 @@
+    
+// Challenge 1: Create an Age Calculator
+var birthyear = 1999;
+var futureyear = 2029;
+var age = futureyear - birthyear;
+console.log(age);
+
+// Challenge 2: Create an information card
+let information = {
+    name: 'Wachirawit',
+    location: 'Ratchaburi',
+    hobby: 'listen podcast'
+}
+console.log(`Hi, my name is ${information.name}. I live in ${information.location} and enjoy ${information.hobby}`);
+
+
+
+
+
+
